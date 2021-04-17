5084310443f0fcf195bcb710c90a9f34189e885f
rahhi555 <whowhose0904@gmail.com>
2021-04-17 14:23:15 +0000

    fix entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/ash
 set -e
 
-cd $APP_ROOT
-echo APP_ROOT: $APP_ROOT
+echo APP_HOME: $APP_HOME
 echo BASE_URL: $BASE_URL
 echo BROWSER_BASE_URL: $BROWSER_BASE_URL
 echo FIREBASE_AUTH_KEY: $FIREBASE_AUTH_KEY
 echo FIREBASE_ADMIN_KEY: $FIREBASE_ADMIN_KEY
-cd /app/hairsalon_bayashi_front
+
+cd $APP_HOME
 npm run start
