0663af60bf4b7011c2ad790234fb46ae83fa017d
rahhi555 <whowhose0904@gmail.com>
2021-04-18 06:16:49 +0000

    commit

--- plugins/firebase.js
+++ plugins/firebase.js
@@ -1,7 +1,16 @@
 import firebase from 'firebase/app'
 import 'firebase/auth'
 
-const fbConfig = JSON.parse(process.env.FIREBASE_AUTH_KEY)
+const fbConfig = {
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  databaseURL: process.env.DATABASE_URL,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGE_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID,
+}
 
 if (!firebase.apps.length) {
   firebase.initializeApp(fbConfig)
