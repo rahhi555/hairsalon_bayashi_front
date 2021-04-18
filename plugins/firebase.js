ddea557f7a900ec37a4fde910fd02b435ce10bbf
rahhi555 <whowhose0904@gmail.com>
2021-04-18 14:30:51 +0000

    env fix

--- plugins/firebase.js
+++ plugins/firebase.js
@@ -2,14 +2,14 @@ import firebase from 'firebase/app'
 import 'firebase/auth'
 
 const fbConfig = {
-  apiKey: process.env.API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  databaseURL: process.env.DATABASE_URL,
-  projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
-  messagingSenderId: process.env.MESSAGE_SENDER_ID,
-  appId: process.env.APP_ID,
-  measurementId: process.env.MEASUREMENT_ID,
+  apiKey: 'AIzaSyCb4D8vXOqwbEkK3EThLTyuuQswxaeJn1w',
+  authDomain: 'hairsalonbayashi.firebaseapp.com',
+  databaseURL: 'https://hairsalonbayashi-default-rtdb.firebaseio.com',
+  projectId: 'hairsalonbayashi',
+  storageBucket: 'hairsalonbayashi.appspot.com',
+  messagingSenderId: '407674908648',
+  appId: '1:407674908648:web:7eabdf898e523bd09a7e5d',
+  measurementId: 'G-96PJYPLW3B',
 }
 
 if (!firebase.apps.length) {
